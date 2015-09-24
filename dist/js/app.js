1717a3f94070d2bb1cf50a3d17660bd67c31f86b
mxl2 <marian.lumba@nreca.coop>
2015-09-24 16:34:28 -0400

    add styles for taskbar and attempt to reverse tasks again

--- dist/js/app.js
+++ dist/js/app.js
@@ -157,9 +157,23 @@ app.factory('submitTask', ['$firebaseArray', function($firebaseArray) {
 
   var ref = new Firebase("https://pomipomi.firebaseio.com/tasks"); //Firebase reference
   var taskList = [];
-  taskList =  $firebaseArray(ref); //AngularFire reference to data
-  //var taskList = [];
-  //taskList = sync.$firebaseArray(ref); //downloads tasks into local array
+  taskList =  $firebaseArray(ref); //AngularFire reference to data; downloads tasks into local array
+
+  /*ref.endAt().limit(100).on('value', update);
+
+  var reverseTasks = function(snap){
+    var list = [];
+    snap.forEach(function(ss) {
+       var data = ss.val();
+       data['.priority'] = ss.getPriority();
+       data['.name'] = ss.name();
+       list.unshift(data);
+    });
+  }*/
+
+  ref.child('tasks').startAt().limitToFirst(20).on('child_added', function(fbdata) {
+    console.log(fbdata.exportVal());
+  })
 
   var getTasks = function (){
     return taskList;
